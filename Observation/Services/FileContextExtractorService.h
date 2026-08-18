840fc76c08b7197827f3c7c8e8d7e1643f5c706d
Ali Pourkarim <46225551+pourkarim1385@users.noreply.github.com>
2026-08-18 19:38:55 +0330

    Adding: Filter Support to the Service

--- Observation/Services/FileContextExtractorService.h
+++ Observation/Services/FileContextExtractorService.h
@@ -9,6 +9,7 @@ private:
     std::vector<std::string> excludesName;
 
 public:
+    fileIncludeFilter();
     explicit fileIncludeFilter(const std::string& name) : filterName(name) {};
     void addExcludeName(std::string name);
     void addExcludeExtension(std::string extension);
@@ -32,13 +33,13 @@ class FileContextExtractorService {
     std::vector<FileSystemEntry> neighboursListing(const std::string& path);
     std::vector<FileSystemEntry> entriesTreeListing(const std::string& path);
 
-    bool filter(FileSystemEntry& file);
+    bool filter(FileSystemEntry& file, fileIncludeFilter targetFilter = fileIncludeFilter());
 
     public:
     static FileContextExtractorService& getInstance();
     FileContextExtractorService(const FileContextExtractorService&) = delete;
     FileContextExtractorService& operator=(const FileContextExtractorService&) = delete;
-    FileContextState getCurrentState(const std::string& filePath);
+    FileContextState getCurrentState(const std::string& filePath, const fileIncludeFilter targetFilter = fileIncludeFilter());
 };
 
 #endif //ACCESSIBILITYSERVICE_FILECONTEXTEXTRACTORSERVICE_H
