9b6dfc8c61966c63f79edb73dd8fc4dfbf468fb8
Ali Pourkarim <46225551+pourkarim1385@users.noreply.github.com>
2026-08-24 14:46:27 +0330

    Implementing Action Dispatcher

---
+++ Actuation/ActionDispatcher.cpp
@@ -0,0 +1,101 @@
+#include "ActionDispatcher.h"
+// #include "Services/InputService.h"
+// #include "Services/FileService.h"
+// #include "Services/SystemService.h"
+// #include "Services/ControlService.h"
+#include "ActionExecutorServices/MouseService.hpp"
+#include "Observation/Services/WorldStateBuilderService.h"
+
+ActionStatus ActionDispatcher::dispatch(const Actions::Action& action) {
+    return std::visit(Actions::Overloaded{
+            [](const Actions::InputData& input)     { return dispatchInput(input); },
+            [](const Actions::FileData& file)       { return dispatchFile(file); },
+            [](const Actions::SystemData& system)   { return dispatchSystem(system); },
+            [](const Actions::ControlData& control) { return dispatchControl(control); }
+    }, action);
+}
+
+ActionStatus ActionDispatcher::dispatchInput(const Actions::InputData& input) {
+    return std::visit(Actions::Overloaded{
+            [](const Actions::MoveMouse& m)   {
+                try{
+                    MouseService::getInstance().moveMouse(m.x, m.y);
+                    return ActionStatus::Success;
+                }
+                catch(...){
+                    return ActionStatus::Failed;
+                }
+                // return InputService::getInstance().moveMouse(m.x, m.y) ? ActionStatus::Success : ActionStatus::Failed;
+            },
+            [](const Actions::Click& c)       {
+                try{
+                    MouseService::getInstance().clickMouse(c.button);
+                    return ActionStatus::Success;
+                }
+                catch(...){
+                    return ActionStatus::Failed;
+                }
+                },
+            [](const Actions::DoubleClick& d) { /* return InputService::getInstance().doubleClick(d.button); */ return ActionStatus::Success; },
+            [](const Actions::Type& t)        { /* return InputService::getInstance().typeText(t.text); */ return ActionStatus::Success; },
+            [](const Actions::KeyPress& k)    { /* return InputService::getInstance().keyPress(k.key); */ return ActionStatus::Success; },
+            [](const Actions::Scroll& s)      { /* return InputService::getInstance().scroll(s.amount); */ return ActionStatus::Success; },
+            [](const Actions::Hotkey& h)      { /* return InputService::getInstance().hotkey(h.keys); */ return ActionStatus::Success; },
+            [](const Actions::MouseDown& md)  { /* return InputService::getInstance().mouseDown(md.button); */ return ActionStatus::Success; },
+            [](const Actions::MouseUp& mu)    { /* return InputService::getInstance().mouseUp(mu.button); */ return ActionStatus::Success; },
+            [](const Actions::DragMouse& dm)  { /* return InputService::getInstance().dragMouse(dm.start_x, dm.start_y, dm.end_x, dm.end_y); */ return ActionStatus::Success; }
+    }, input);
+}
+
+ActionStatus ActionDispatcher::dispatchFile(const Actions::FileData& file) {
+    return std::visit(Actions::Overloaded{
+            [](const Actions::CreateFile& c) { /* return FileService::getInstance().createFile(c.path); */ return ActionStatus::Success; },
+            [](const Actions::WriteFile& w)  { /* return FileService::getInstance().writeFile(w.path, w.text); */ return ActionStatus::Success; },
+            [](const Actions::AppendFile& a) { /* return FileService::getInstance().appendFile(a.path, a.text); */ return ActionStatus::Success; },
+            [](const Actions::InsertFile& i) { /* return FileService::getInstance().insertFile(i.path, i.position, i.text); */ return ActionStatus::Success; },
+            [](const Actions::DeleteFile& d) { /* return FileService::getInstance().deleteFile(d.path); */ return ActionStatus::Success; },
+            [](const Actions::RenameFile& r) { /* return FileService::getInstance().renameFile(r.path, r.new_path); */ return ActionStatus::Success; },
+            [](const Actions::CopyFile& c)   { /* return FileService::getInstance().copyFile(c.path, c.destination); */ return ActionStatus::Success; },
+            [](const Actions::MoveFile& m)   { /* return FileService::getInstance().moveFile(m.path, m.destination); */ return ActionStatus::Success; },
+            [](const Actions::EditFile& ef)   { /* return FileService::getInstance().moveFile(m.path, m.destination); */ return ActionStatus::Success; }
+    }, file);
+}
+
+ActionStatus ActionDispatcher::dispatchSystem(const Actions::SystemData& system) {
+    return std::visit(Actions::Overloaded{
+            [](const Actions::RunCmd& r)         { return SystemService::getInstance().runCommand(r); },
+            [](const Actions::RunPowerShell& r)  { return SystemService::getInstance().runPowerShell(r); },
+            [](const Actions::OpenApp& o)        { /* return SystemService::getInstance().openApp(o.name); */ return ActionStatus::Success; },
+            [](const Actions::CloseApp& c)       { /* return SystemService::getInstance().closeApp(c.name); */ return ActionStatus::Success; },
+            [](const Actions::FocusWindow& f)    { /* return SystemService::getInstance().focusWindow(f.name); */ return ActionStatus::Success; },
+            [](const Actions::MinimizeWindow& m) { /* return SystemService::getInstance().minimizeWindow(m.name); */ return ActionStatus::Success; },
+            [](const Actions::MaximizeWindow& m) { /* return SystemService::getInstance().maximizeWindow(m.name); */ return ActionStatus::Success; },
+            [](const Actions::RestoreWindow& r)  { /* return SystemService::getInstance().restoreWindow(r.name); */ return ActionStatus::Success; },
+            [](const Actions::SetVolume& s)      { /* return SystemService::getInstance().setVolume(s.value); */ return ActionStatus::Success; },
+            [](const Actions::MuteVolume& m)     { /* return SystemService::getInstance().muteVolume(); */ return ActionStatus::Success; },
+            [](const Actions::UnmuteVolume& u)   { /* return SystemService::getInstance().unmuteVolume(); */ return ActionStatus::Success; },
+            [](const Actions::Sleep& s)          { /* return SystemService::getInstance().sleep(); */ return ActionStatus::Success; },
+            [](const Actions::Shutdown& s)       { /* return SystemService::getInstance().shutdown(); */ return ActionStatus::Success; },
+            [](const Actions::Restart& r)        { /* return SystemService::getInstance().restart(); */ return ActionStatus::Success; }
+    }, system);
+}
+
+ActionStatus ActionDispatcher::dispatchControl(const Actions::ControlData& control) {
+    return std::visit(Actions::Overloaded{
+            [](const Actions::Msg& m)        { /* return ControlService::getInstance().showMessage(m.content); */ return ActionStatus::Success; },
+
+            // SPECIAL CASE: The Observe action returns a specific status to break the orchestrator loop
+            [](const Actions::Observe& o)    {
+                /* ControlService::getInstance().triggerObservePrep(); */
+                return ActionStatus::TriggerObserve;
+            },
+
+            [](const Actions::Wait& w)       { /* return ControlService::getInstance().wait(w.value); */ return ActionStatus::Success; },
+            [](const Actions::FAR& r)        {
+                bool status = WorldStateBuilderService::getInstance().fileAnalyzeRequest(r.path);
+                return (status) ? ActionStatus::Success : ActionStatus::Failed;
+                },
+            [](const Actions::IsVerified& i) { /* return ControlService::getInstance().setVerified(i.value); */ return ActionStatus::Success; },
+            [](const Actions::ClearStack& c) { /* return ControlService::getInstance().clearStack(); */ return ActionStatus::Success; }
+    }, control);
+}
