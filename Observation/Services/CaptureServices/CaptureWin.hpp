8ac2b37ab887e665064acdccbc039fc65da39ca2
Amirreza.S.Nasiri <seyywdnasiriamirreza@gmail.com>
2026-08-25 17:03:43 +0330

    The Win Capture can save the images with the base64 format.

--- Observation/Services/CaptureServices/CaptureWin.hpp
+++ Observation/Services/CaptureServices/CaptureWin.hpp
@@ -1,6 +1,11 @@
 #pragma once
+
+#include <fstream>
+#include <string>
 #include <vector>
 #include <cstdint>
 #include "../../Public.h"
 
 std::vector<uint8_t> captureScreenshotWindows(ImageFormat& OUT_fmt, ImageFormat fmt = ImageFormat::PNG, int quality = 75);
+static void compositeCursor(std::vector<unsigned char>& rgb, int width, int height);
+static std::string base64_encode(const std::vector<unsigned char>& data);
