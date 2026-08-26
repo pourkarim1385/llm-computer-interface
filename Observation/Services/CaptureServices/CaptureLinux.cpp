eb0d1afe83ff24f40f9ea8e160768b2c4360c30f
Amirreza.S.Nasiri <seyywdnasiriamirreza@gmail.com>
2026-08-26 19:55:47 +0330

    Revert "The linux CApture and saves Base64 format for sending api."
    This reverts commit 08e1b0827afbb728b46da9ae2543a1500befc034.

--- Observation/Services/CaptureServices/CaptureLinux.cpp
+++ Observation/Services/CaptureServices/CaptureLinux.cpp
@@ -1,44 +1,9 @@
 #include "CaptureLinux.hpp"
-#include <fstream>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../../Models/stb_image_write.h"
 
-static const char b64_table[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-static std::string base64_encode(const std::vector<unsigned char>& data) {
-    std::string out;
-    out.reserve(((data.size() + 2) / 3) * 4);
-
-    size_t i = 0;
-    while (i + 3 <= data.size()) {
-        unsigned int n = (data[i] << 16) | (data[i+1] << 8) | data[i+2];
-        out += b64_table[(n >> 18) & 0x3F];
-        out += b64_table[(n >> 12) & 0x3F];
-        out += b64_table[(n >> 6)  & 0x3F];
-        out += b64_table[n & 0x3F];
-        i += 3;
-    }
-
-    size_t rem = data.size() - i;
-    if (rem == 1) {
-        unsigned int n = data[i] << 16;
-        out += b64_table[(n >> 18) & 0x3F];
-        out += b64_table[(n >> 12) & 0x3F];
-        out += "==";
-    } else if (rem == 2) {
-        unsigned int n = (data[i] << 16) | (data[i+1] << 8);
-        out += b64_table[(n >> 18) & 0x3F];
-        out += b64_table[(n >> 12) & 0x3F];
-        out += "=";
-    }
-
-    return out;
-}
-
-std::vector<unsigned char> captureScreenshotLinux(ImageFormat& OUT_fmt, ImageFormat fmt,
-                                                   const std::string& outTxtPath) {
+std::vector<unsigned char> captureScreenshotLinux(ImageFormat& OUT_fmt, ImageFormat fmt) {
     Display* display = XOpenDisplay(nullptr);
     Window root = DefaultRootWindow(display);
     XWindowAttributes attrs;
@@ -61,26 +26,17 @@ std::vector<unsigned char> captureScreenshotLinux(ImageFormat& OUT_fmt, ImageFor
     XDestroyImage(img);
     XCloseDisplay(display);
 
-    std::vector<unsigned char> raw;
+    std::vector<unsigned char> out;
     auto writer = [](void* ctx, void* data, int size) {
         auto* v = static_cast<std::vector<unsigned char>*>(ctx);
         v->insert(v->end(), (unsigned char*)data, (unsigned char*)data + size);
     };
 
     if (fmt == ImageFormat::PNG)
-        stbi_write_png_to_func(writer, &raw, width, height, 3, rgb.data(), width * 3);
+        stbi_write_png_to_func(writer, &out, width, height, 3, rgb.data(), width * 3);
     else
-        stbi_write_jpg_to_func(writer, &raw, width, height, 3, rgb.data(), 90);
+        stbi_write_jpg_to_func(writer, &out, width, height, 3, rgb.data(), 90);
 
     OUT_fmt = fmt;
-
-    std::string b64 = base64_encode(raw);
-
-    std::ofstream out(outTxtPath, std::ios::out | std::ios::trunc);
-    out << b64;
-    out.close();
-
-    std::vector<unsigned char> b64Vec(b64.begin(), b64.end());
-    return b64Vec;
-
+    return out;
 }
--- Observation/Services/CaptureServices/CaptureLinux.hpp
+++ Observation/Services/CaptureServices/CaptureLinux.hpp
@@ -9,6 +9,4 @@
 #include <X11/extensions/Xfixes.h>
 #include "../../Public.h"
 
-std::vector<unsigned char> captureScreenshotLinux(ImageFormat& OUT_fmt, ImageFormat fmt,
-                                                   const std::string& outTxtPath);
-static std::string base64_encode(const std::vector<unsigned char>& data);
+std::vector<uint8_t> captureScreenshotLinux(ImageFormat& OUT_fmt, ImageFormat fmt = ImageFormat::PNG);
