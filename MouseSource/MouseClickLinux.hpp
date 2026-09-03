6b445791a61c3edb09ca3a9c84a76b85259ce1cd
Ali Pourkarim <46225551+pourkarim1385@users.noreply.github.com>
2026-09-03 12:34:24 +0330

    Moved MouseServices to ActionDTOS

--- MouseSource/MouseClickLinux.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MOUSE_CLICK_LINUX_HPP
-#define MOUSE_CLICK_LINUX_HPP
-
-void clickMouseLeft();
-void clickMouseRight();
-void clickMouseMiddle();
-void mouseButtonDown(unsigned int button);
-void mouseButtonUp(unsigned int button);
-#endif
