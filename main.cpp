d22a9a06209f9324a29583c9f0a255a30a84123c
Ali Pourkarim <46225551+pourkarim1385@users.noreply.github.com>
2026-09-08 21:12:06 +0330

    Updating tempMain accordingly

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 
 #include "Core/Orchestrator.h"
 #include "Repository/DatabaseManager.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "Front-end/Controllers/InputBoxController.h"
+#include "Front-end/Models/ChatListModel.h"
+#include "Front-end/Controllers/NavigationController.h"
+#include "Front-end/Models/InputBoxModel.h"
+#include "Front-end/AgentBridge.h"
 
 using namespace std;
-
+/*
 int main(int argc, const char * argv[]) {
     cout << "[System] Initializing database...\n";
     string apiKey = "sk-OzgzQqIc8azSnEH9Lzn5EYx1mLabqH2tizw99nVWGdTD0KE3";
@@ -39,4 +48,80 @@ int main(int argc, const char * argv[]) {
             orchestrator.handleUserPrompt(userInput);
         }
     }
+}
+*/
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <memory>
+
+#include "Core/Orchestrator.h"
+#include "Repository/DatabaseManager.h"
+#include "Front-end/AgentBridge.h"
+
+#include "Front-end/Controllers/InputBoxController.h"
+#include "Front-end/Models/ChatListModel.h"
+#include "Front-end/Controllers/NavigationController.h"
+#include "Front-end/Models/InputBoxModel.h"
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    string apiKey = "sk-OzgzQqIc8azSnEH9Lzn5EYx1mLabqH2tizw99nVWGdTD0KE3";
+    string endpoint = "https://api.gapgpt.app/v1/chat/completions";
+    agent::config::LLMProviderConfig myConfig = agent::config::LLMProviderConfig("1", "gapgpt", "nigger", endpoint, apiKey, agent::config::ApiFormat::OpenAICompatible);
+    agent::settings::UserSettings mySetting = agent::settings::UserSettings("aliAndReza", "random", "i am not epstien");
+    mySetting.addProvider(myConfig);
+    mySetting.setActiveProviderId("1");
+
+    agent::repository::DatabaseManager::getInstance().initialize("agent_data.db");
+    auto& repoManager = agent::repository::RepositoryManager::getInstance();
+    repoManager.settings().saveSettings(mySetting);
+
+    auto orchestrator = std::make_shared<Orchestrator>();
+    AgentBridge agentBridge(orchestrator);
+
+    ChatListModel chatModel;
+    NavigationController navController(&agentBridge);
+    InputBoxController inputController(&agentBridge);
+    InputBoxModel inputModel;
+
+    QObject::connect(&chatModel, &ChatListModel::chatSelected,
+                     &agentBridge, &AgentBridge::setActiveChat);
+
+    QObject::connect(&agentBridge, &AgentBridge::chatSessionLoaded,
+                     &chatModel, &ChatListModel::addAndSelectChat);
+
+    QObject::connect(&agentBridge, &AgentBridge::chatsLoaded,
+                     &chatModel, &ChatListModel::setChats);
+
+    agentBridge.loadChatsFromRepository();
+
+    if (chatModel.rowCount() == 0) {
+        orchestrator->createNewChat();
+    } else {
+        chatModel.selectChat(0);
+    }
+
+    QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("agentBridge", &agentBridge);
+    engine.rootContext()->setContextProperty("chatModel", &chatModel);
+    engine.rootContext()->setContextProperty("navController", &navController);
+    engine.rootContext()->setContextProperty("inputBoxController", &inputController);
+    engine.rootContext()->setContextProperty("inputBoxModel", &inputModel);
+
+    const QUrl url(QStringLiteral("qrc:/AccessibilityService/Front-end/Main.qml"));
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+    //engine.loadFromModule("AccessibilityService", "Main");
+
+    return app.exec();
 }
