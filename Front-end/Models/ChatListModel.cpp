1aef9cdde86f0bb592e64228aed6b844304ddb95
Ali Pourkarim <46225551+pourkarim1385@users.noreply.github.com>
2026-09-10 00:46:17 +0330

    Adding Rename/Delete methods to ChatList

--- Front-end/Models/ChatListModel.cpp
+++ Front-end/Models/ChatListModel.cpp
@@ -1,7 +1,9 @@
 #include "ChatListModel.h"
+#include "Front-end/AgentBridge.h"
+#include <QDebug>
 
-ChatListModel::ChatListModel(QObject *parent) : QAbstractListModel(parent) {
-    //
+ChatListModel::ChatListModel(AgentBridge *bridge, QObject *parent)
+    : QAbstractListModel(parent), m_bridge(bridge) {
 }
 
 int ChatListModel::rowCount(const QModelIndex &parent) const {
@@ -70,6 +72,16 @@ void ChatListModel::selectChat(int index) {
     if (m_activeIndex == index)
         return;
 
+    if (m_bridge && m_bridge->isWorking()) {
+        qDebug() << "[ChatListModel] Agent is busy. Switch rejected.";
+        return;
+    }
+
+    if (m_bridge) {
+        const bool success = m_bridge->setActiveChat(m_chats[index].id);
+        if (!success) return;
+    }
+
     if (m_activeIndex >= 0 && m_activeIndex < m_chats.size()) {
         m_chats[m_activeIndex].isActive = false;
         QModelIndex prev = createIndex(m_activeIndex, 0);
@@ -88,4 +100,60 @@ QString ChatListModel::activeChatId() const {
     if (m_activeIndex >= 0 && m_activeIndex < m_chats.size())
         return m_chats[m_activeIndex].id;
     return QString();
+}
+
+bool ChatListModel::deleteChat(const QString &chatId) {
+    if (m_bridge) {
+        if (!m_bridge->deleteChat(chatId)) {
+            return false;
+        }
+    }
+    return removeRowById(chatId);
+}
+
+bool ChatListModel::renameChat(const QString &chatId, const QString &newTitle) {
+    QString trimmed = newTitle.trimmed();
+    if (trimmed.isEmpty()) return false;
+
+    if (m_bridge) {
+        if (!m_bridge->renameChat(chatId, trimmed)) {
+            return false;
+        }
+    }
+    updateTitleInMemory(chatId, trimmed);
+    return true;
+}
+
+bool ChatListModel::removeRowById(const QString &chatId) {
+    int targetIndex = -1;
+    for (int i = 0; i < m_chats.size(); ++i) {
+        if (m_chats[i].id == chatId) {
+            targetIndex = i;
+            break;
+        }
+    }
+    if (targetIndex < 0) return false;
+
+    beginRemoveRows(QModelIndex(), targetIndex, targetIndex);
+    bool wasActive = m_chats[targetIndex].isActive;
+    m_chats.removeAt(targetIndex);
+
+    if (wasActive) {
+        m_activeIndex = -1;
+    } else if (m_activeIndex > targetIndex) {
+        m_activeIndex--;
+    }
+    endRemoveRows();
+    return true;
+}
+
+void ChatListModel::updateTitleInMemory(const QString &chatId, const QString &newTitle) {
+    for (int i = 0; i < m_chats.size(); ++i) {
+        if (m_chats[i].id == chatId) {
+            m_chats[i].title = newTitle;
+            QModelIndex idx = createIndex(i, 0);
+            emit dataChanged(idx, idx, {TitleRole});
+            break;
+        }
+    }
 }
--- Front-end/Models/ChatListModel.h
+++ Front-end/Models/ChatListModel.h
@@ -3,6 +3,8 @@
 #include <QList>
 #include "ChatItem.h"
 
+class AgentBridge;
+
 class ChatListModel : public QAbstractListModel {
     Q_OBJECT
 
@@ -13,7 +15,7 @@ public:
         IsActiveRole
     };
 
-    explicit ChatListModel(QObject *parent = nullptr);
+    explicit ChatListModel(AgentBridge *bridge, QObject *parent = nullptr);
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
@@ -21,6 +23,9 @@ public:
 
     Q_INVOKABLE void selectChat(int index);
     Q_INVOKABLE QString activeChatId() const;
+
+    Q_INVOKABLE bool deleteChat(const QString &chatId);
+    Q_INVOKABLE bool renameChat(const QString &chatId, const QString &newTitle);
 
 public slots:
     void setChats(const QList<ChatItem> &chats);
@@ -30,6 +35,10 @@ public slots:
         void chatSelected(const QString &chatId);
 
 private:
+    bool removeRowById(const QString &chatId);
+    void updateTitleInMemory(const QString &chatId, const QString &newTitle);
+
+    AgentBridge *m_bridge = nullptr;
     QList<ChatItem> m_chats;
     int m_activeIndex = -1;
 };
